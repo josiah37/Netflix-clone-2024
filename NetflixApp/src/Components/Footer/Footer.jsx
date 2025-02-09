1ca29a55b47b43f060498d1792e18eac5196aa76
josiah37 <eyosi50@gmail.com>
2025-02-09 23:11:10 +0300

    adding footer and header component

---
+++ NetflixApp/src/Components/Footer/Footer.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+
+const Footer = () => {
+   return (
+      <>
+         <div className={styles.Footer}>
+            <ul>
+               <li>
+                  <FacebookOutlinedIcon />
+               </li>
+               <li>
+                  <InstagramIcon />
+               </li>
+               <li>
+                  <TwitterIcon />
+               </li>
+               <li>
+                  <YouTubeIcon />
+               </li>
+            </ul>
+            <div className={styles.footerLinks}>
+               <div className={styles.firstCol}>
+                  <li>
+                     <a href="http://">Audio discription</a>
+                  </li>
+                  <li>
+                     <a href="http://">investor relation</a>
+                  </li>
+                  <li>
+                     <a href="http://">Legal notice</a>
+                  </li>
+                  <li>
+                     <a href="http://">Service Code</a>
+                  </li>
+               </div>
+
+               <div className={styles.secCol}>
+                  <li>
+                     <a href="http://">Help center</a>
+                  </li>
+                  <li>
+                     <a href="http://">Jobs</a>
+                  </li>
+                  <li>
+                     <a href="http://">cookies Prefernces</a>
+                  </li>
+               </div>
+
+               <div className={styles.thirdCol}>
+                  <li>
+                     <a href="http://">Media center</a>
+                  </li>
+                  <li>
+                     <a href="http://">Privacy</a>
+                  </li>
+                  <li>
+                     <a href="http://">ContactUs</a>
+                  </li>
+               </div>
+            </div>
+            <p>&copy; 1997-2004 Netflix, Inc.</p>
+         </div>
+      </>
+   );
+};
+
+export default Footer;
---
+++ NetflixApp/src/Components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import netflixLogo from "../../assets/netflix-3.svg";
+import styles from "./Header.module.css";
+import PersonIcon from "@mui/icons-material/Person";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+const Header = () => {
+   return (
+      <>
+         <div className={styles.Header}>
+            <nav>
+               <img src={netflixLogo} width="70px" alt="netflix Logo" />
+               <ul>
+                  <li>Home</li>
+                  <li>Tv shows</li>
+                  <li>Movies</li>
+                  <li>Latest</li>
+                  <li>My List</li>
+                  <li>Browse by Languages</li>
+               </ul>
+            </nav>
+
+            <div className={styles.rightIcons}>
+               <li>
+                  <PersonIcon />
+               </li>
+               <li>
+                  <SearchIcon />
+               </li>
+               <li>
+                  <NotificationsIcon />
+               </li>
+               <li>
+                  <PersonIcon />
+               </li>
+               <li>
+                  <ArrowDropDownIcon />
+               </li>
+            </div>
+         </div>
+      </>
+   );
+};
+
+export default Header;
